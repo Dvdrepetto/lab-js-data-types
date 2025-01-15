d640294efdab0aa5a7ba64dc75ede24b161aa8b2
Dvdrepetto <davidmrepetto@gmail.com>
2025-01-15 16:25:32 +0200

    solved lab

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+let tongueTwister = s1 +" "+ s2 +" "+  s3 +" "+  s4 +" "+  s5 +" "+ s3 +" "+ s2 +" "+ s1+" "+ s4;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -23,9 +24,15 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const lastLetterPart1 = part1.slice(-1).toUpperCase();
+const lastLetterPart2 = part2.slice(-1).toUpperCase();
 
+const newStr1 = part1.slice(0, -1) + lastLetterPart1;
+const newStr2 = part2.slice(0, -1) + lastLetterPart2;
+
+const result = newStr1 + newStr2;
 // Print the cameLtaiL-formatted string
-
+console.log(result)
 
 
 
@@ -35,11 +42,12 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tipAmount = billTotal * 15 / 100;
+let roundTip = Math.ceil(tipAmount)
 
 // Print out the tipAmount
-
-
+console.log(tipAmount);
+console.log(roundTip);
 
 
 /*******************************************
@@ -48,10 +56,13 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-
+function randomNumber(min,max) {
+return Math.random() * (max - min) + min;
+}
+const numbers = randomNumber(1 , 10);
 // Print the generated random number
 
-
+console.log(Math.ceil(numbers))
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +73,16 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+console.log(expression1) //false
 const expression2 = a || b;
-
+console.log(expression2) //true
 const expression3 = !a && b;
-
+console.log(expression3) //false
 const expression4 = !(a && b);
-
+console.log(expression4)// true
 const expression5 = !a || !b;
-
+console.log(expression5) //true
 const expression6 = !(a || b);
-
+console.log(expression6) //false
 const expression7 = a && a;
+console.log(expression7)// true
